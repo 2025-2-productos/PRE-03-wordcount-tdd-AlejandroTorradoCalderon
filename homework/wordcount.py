631bad50a442c7df492e46bd45f018843195fcea
AlejandroTorradoCalderon <112442070+AlejandroTorradoCalderon@users.noreply.github.com>
2025-11-29 12:43:33 -0500

    demás funciones y tests

--- homework/wordcount.py
+++ homework/wordcount.py
@@ -7,6 +7,8 @@
 import argparse
 import sys
 
+from homework.src._internals import read_all_lines
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Count Word in files.")
@@ -24,7 +26,26 @@ def parse_args():
     return parsed_args.input, parsed_args.output
 
 
+def preprocess_lines(lines):
+    pass
+
+
+def split_into_words(preprocessed_lines):
+    pass
+
+
+def count_words(words):
+    pass
+
+
+def write_word_counts(output_folder, word_counts):
+    pass
+
+
 def main():
-    input, output = parse_args()
-    print(f"input: {input}")
-    print(f"output: {output}")
+    input_folder, output_folder = parse_args()
+    lines = read_all_lines(input_folder)
+    preprocessed_lines = preprocess_lines(lines)
+    words = split_into_words(preprocessed_lines)
+    word_counts = count_words(words)
+    write_word_counts(output_folder, word_counts)
